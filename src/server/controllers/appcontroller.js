2bb957e8c405056d19d1bfa80bd4158f2b4623bb
Selvaganesh <selvaganeshrajam@gmail.com>
2019-01-24 23:31:21 +0530

    server on the fly build

--- src/server/controllers/appcontroller.js
+++ src/server/controllers/appcontroller.js
@@ -1,33 +1,33 @@
 export function initialize(req, res) {
-  res.send("OK");
+  res.send('OK')
 }
 
 export function requestValidation(req, res, next) {
-  let authToken = req.headers["authtoken"];
+  let authToken = req.headers['authtoken']
   if (!req.headers.authtoken) {
     res.status(403).send({
-      status: "success",
+      status: 'success',
       payload: {
-        message: "No credentials sent"
+        message: 'No credentials sent'
       }
-    });
+    })
   } else if (authToken === undefined || authToken != process.env.AUTH_TOKEN) {
     res.status(401).send({
-      status: "success",
+      status: 'success',
       payload: {
-        message: "Not valid access token"
+        message: 'Not valid access token'
       }
-    });
+    })
   } else {
-    next();
+    next()
   }
 }
 
 export function getMe(req, res) {
   res.json({
-    status: "success",
+    status: 'success',
     payload: {
-      message: "Message from Backend Express JS"
+      message: 'Message from Backend Express JS'
     }
-  });
+  })
 }
