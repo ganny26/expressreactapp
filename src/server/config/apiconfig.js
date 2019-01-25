d4607ee5cb55a4d23be0816a6b7b54a8ce3c3549
Selvaganesh <selvaganeshrajam@gmail.com>
2019-01-25 22:34:25 +0530

    seperate global config

--- src/server/config/apiconfig.js
+++ src/server/config/apiconfig.js
@@ -3,37 +3,45 @@ import bodyparser from 'body-parser';
 import uuid from 'uuid';
 import cors from 'cors';
 import morgan from 'morgan';
-
+import db from '../models';
 const app = express();
 
 //http request reponse logging
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      req.headers['authtoken'],
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    req.headers['authtoken'],
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
 
 
 app.disable('x-powered-by');
 
 const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 200
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 200
 }
 app.use(cors(corsOptions))
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json());
 
+db.sequelize.authenticate().then(() => {
+  console.log('MYSQL Connection has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
-app.use((req,res,next)=>{
-    res.set('X-My-Access-Token',uuid.v1());
-    next();
+// db.sequelize.sync().then(result=>console.log('synced')).catch(err=>console.log('error while synciing'))
+
+
+app.use((req, res, next) => {
+  res.set('X-My-Access-Token', uuid.v1());
+  next();
 })
 
 module.exports = app;
